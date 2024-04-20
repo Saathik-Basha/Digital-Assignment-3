4fa06880d1a8917ea3683f628e75c970135c38fd
Saathik-Basha <155795562+Saathik-Basha@users.noreply.github.com>
2024-04-20 15:07:46 +0530

    Add files via upload

---
+++ Digital Assignment - 3/script.js
@@ -0,0 +1,61 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+let mode = "easy"; 
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    let minTime, maxTime;
+    if (mode === "easy") {
+        minTime = 500;
+        maxTime = 1000;
+    } else if (mode === "medium") {
+        minTime = 300;
+        maxTime = 800;
+    }
+    const time = randomTime(minTime, maxTime);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+}
+
+function whack(e) {
+    if (!e.isTrusted) return;
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', whack));
+
+function setMode(selectedMode) {
+    mode = selectedMode;
+    startGame();
+}
